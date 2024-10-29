bad725b1a0e9f2be65267bc9a971c40338cff8f0
Henoch <henochyanuar13@gmail.com>
2024-10-29 23:29:58 +0700

    chore(util): create a function to declire an id

--- utils/error.js
+++ utils/error.js
@@ -2,3 +2,12 @@ const err500 = {
   message : '500 | Internal Server Error',
   layout : 'error/error'
 }
+
+const err404 = {
+ message : '404 | Page Not Found',
+ layout : 'error/error'
+}
+
+module.exports = {
+  err500, err404
+}
