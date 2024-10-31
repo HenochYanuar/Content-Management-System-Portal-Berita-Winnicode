0cf963f8055728badd71d9a732ea21e913441ac9
Henoch <henochyanuar13@gmail.com>
2024-10-31 22:06:24 +0700

    chore(database): change the data type of the content field

---
+++ migrations/20241031080539_alter_articles_content.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.table('articles', (table) => {
+    table.text('content').alter();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.table('articles', (table) => {
+    table.string('content').alter();
+  });
+};
