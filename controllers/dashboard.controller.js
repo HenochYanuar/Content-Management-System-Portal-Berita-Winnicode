51b68de56c462e6283dec29ce2a0f97a2002bf0d
Henoch <henochyanuar13@gmail.com>
2024-11-01 23:28:59 +0700

    feat(dashboard): add and display the number of articles and users

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -1,17 +1,28 @@
 const userModel = require('../models/user.model')
-const { errMsg, errLayout } = require('../utils/error')
+const articleModel = require('../models/article.model')
+const { err500, err404 } = require('../utils/error')
 const layout = 'layout/index'
 
 const dashboard = async (req, res) => {
-
   try {
     const user = await userModel.findByEmail(req.user.email)
+    const articles = await articleModel.getCountAll()
+
+    const role  = 'user'
+    const userRegist = true
+    const userNotRegist = false
+
+    const users = await userModel.getCountAll(role, userRegist, userNotRegist)
+
+    const context = {
+      user, articles, users
+    }
   
-    res.status(200).render('dashboard/index', { context: { user }, title: 'Dashboard Admin', layout: layout})
+    res.status(200).render('dashboard/index', { context, title: 'Dashboard Admin', layout})
     
   } catch (error) {
     console.error('Error in dashboard:', error)
-    res.status(500).render(errLayout, errMsg)
+    res.status(500).render('error/error', err500)
   }
 }
 
